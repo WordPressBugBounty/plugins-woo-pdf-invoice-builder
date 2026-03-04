ab7789b3b34d5b3e93bcbb6d99a93022f003fd05
Sean Murphy <sean@iamseanmurphy.com>
2026-03-04 19:27:20 +0000

    1.2.156

--- woo-pdf-invoice-builder/jstranslations/metabox.php
+++ woo-pdf-invoice-builder/jstranslations/metabox.php
@@ -1,5 +1,10 @@
 <?php return array(
 "PDF:"=>__("PDF:","wooinvoicebuilder"),
+"Create PDF"=>__("Create PDF","wooinvoicebuilder"),
+"View PDF"=>__("View PDF","wooinvoicebuilder"),
+"Print (full version only)"=>__("Print (full version only)","wooinvoicebuilder"),
+"Email"=>__("Email","wooinvoicebuilder"),
+"Delete"=>__("Delete","wooinvoicebuilder"),
 "You haven't created any PDF template!, to create pdfs please"=>__("You haven't created any PDF template!, to create pdfs please","wooinvoicebuilder"),
 "This pdf hasn't been create for this order"=>__("This pdf hasn't been create for this order","wooinvoicebuilder"),
 "Invoice Number:"=>__("Invoice Number:","wooinvoicebuilder"),
@@ -7,10 +12,5 @@
 "Edit invoice date"=>__("Edit invoice date","wooinvoicebuilder"),
 "Edit invoice number"=>__("Edit invoice number","wooinvoicebuilder"),
 "Sorry this feature is only available in the full version"=>__("Sorry this feature is only available in the full version","wooinvoicebuilder"),
-"Create PDF"=>__("Create PDF","wooinvoicebuilder"),
-"View PDF"=>__("View PDF","wooinvoicebuilder"),
-"Print (full version only)"=>__("Print (full version only)","wooinvoicebuilder"),
-"Email"=>__("Email","wooinvoicebuilder"),
-"Delete"=>__("Delete","wooinvoicebuilder"),
 "Thanks for shopping with us!"=>__("Thanks for shopping with us!","wooinvoicebuilder")
 );
